746293024fbc9a5a42787a5bd165b7ec3724ffce
Carmen Heredia Requena <99763149+chr772@users.noreply.github.com>
2022-03-15 12:11:44 +0100

    ti4

--- TrabajoIndividual4/trabajo4.c
+++ TrabajoIndividual4/trabajo4.c
@@ -11,7 +11,7 @@
 
 int main(){
     /* Declaracion de variables */
-    char c; double x1, y1, z1, a, x2, y2, z2, b, x3, y3, z3, c, x, y, z, mcmx, mcmy, mcmz;
+    char c; double x1, y1, z1, a, x2, y2, z2, b, x3, y3, z3, c, x, y, z;
 
     /* Bloque de instrucciones */
     do{ system("cls");
@@ -21,28 +21,31 @@ int main(){
           printf("\nIntroduzca los términos de la primera ecuación (x+y+z=a)");
           scanf("%lf %lf %lf" ,&x1 ,&y1 ,&z1 ,&a);
           printf("Introduzca los términos de la segunda ecuación");
-          scanf("%lf %lf %lf" ,&x2 ,&y2 ,&z2, ,&b);
+          scanf("%lf %lf %lf" &x2 ,&y2 ,&z2, ,&b);
           printf("Introduzca los términos de la tercera ecuación ");
           scanf("%lf %lf %lf " ,&x3 ,&y3 ,&z3 ,&c);
           /*Reducción de x */
-          while(x2!=0){
+          if(x2!=0){
             x2=x2*x1-x1*x2
             y2=y2*x1-y1*x2
             z2=z2*x1-z1*x2
             b=b*x1-a*x2
           }
-          while (x3!=0){
+
+          if (x3!=0){
             x3=x3*x1-x1*x3
             y3=y3*x1-y1*x3
             z3=z3*x1-z1*x3
             c=c*x1-a*x3
           }
+
           /*Reducción de y */
-          while (y3!=0){
+          if (y3!=0){
             y3=y3*y1-y1*y3
             z3=z3*y1-z1*y3
             c=c*x1-a*x3
           }
+           
           z=c/z3
           y=(b-z)/y2
           x=(a-y-z)/x1
